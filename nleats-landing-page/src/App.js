31ac6d09ac93716b4e69eb4d3e76f09ef6f520e2
aaronoates <89139227+aaronoates@users.noreply.github.com>
2023-10-04 14:18:05 -0230

    ChatGPT recommendations
    Some empty JS files to get us started.

---
+++ nleats-landing-page/src/App.js
@@ -0,0 +1,19 @@
+import react, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to the NLEats Hydroponics website!
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
