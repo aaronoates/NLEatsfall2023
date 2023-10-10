82e40f13d613adf50e6f05e37833b0d3d43e5eaf
aaronoates <89139227+aaronoates@users.noreply.github.com>
2023-10-10 17:03:36 -0230

    Landing page updates
    added files according to what figma anima recommended.

---
+++ nleats-landing-page/src/screens/Index/Index.jsx
@@ -0,0 +1,285 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="overlap">
+          <div className="NAVBAR">
+            <div className="text-wrapper">NL Eats</div>
+            <div className="overlap-group">
+              <div className="text-wrapper-2">Home</div>
+              <div className="text-wrapper-3">About</div>
+              <div className="text-wrapper-4">Projects</div>
+              <button className="button">
+                <div className="overlap-group-2">
+                  <div className="rectangle" />
+                  <div className="text-wrapper-5">Sign Up</div>
+                </div>
+              </button>
+            </div>
+            <img
+              className="nleats"
+              alt="Nleats"
+              src="https://cdn.animaapp.com/projects/652581de01736f706d586056/releases/65258fe3619789033b447866/img/nleats-3-1@2x.png"
+            />
+          </div>
+          <div className="overlap-2">
+            <img
+              className="join-the-hydroponics"
+              alt="Join the hydroponics"
+              src="https://cdn.animaapp.com/projects/652581de01736f706d586056/releases/65258fe3619789033b447866/img/join-the-hydroponics-revolution.svg"
+            />
+            <p className="p">Cultivating a Greener Future through Innovative Education</p>
+            <img
+              className="untitled-design"
+              alt="Untitled design"
+              src="https://cdn.animaapp.com/projects/652581de01736f706d586056/releases/65258fe3619789033b447866/img/untitled-design--2--1.png"
+            />
+          </div>
+          <img
+            className="arrow"
+            alt="Arrow"
+            src="https://cdn.animaapp.com/projects/652581de01736f706d586056/releases/65258fe3619789033b447866/img/arrow-1.svg"
+          />
+        </div>
+        <div className="text-wrapper-6">Why NL Eats?</div>
+        <div className="frame">
+          <div className="frame-2">
+            <img
+              className="element-leaf-nature"
+              alt="Element leaf nature"
+              src="https://cdn.animaapp.com/projects/652581de01736f706d586056/releases/65258fe3619789033b447866/img/8665589-leaf-nature-icon-1.svg"
+            />
+            <div className="frame-3">
+              <div className="div-wrapper">
+                <div className="text-wrapper-7">Sustainable Agriculture</div>
+              </div>
+              <p className="text-wrapper-8">
+                Our hydroponic units use up to 90% less water than traditional soil farming, making them a sustainable
+                choice for food production. Plus, they can be used indoors, reducing the need for transportation and
+                associated carbon emissions.
+              </p>
+            </div>
+          </div>
+          <div className="frame-4">
+            <img
+              className="element-leaf-nature"
+              alt="Element leaf nature"
+              src="https://cdn.animaapp.com/projects/652581de01736f706d586056/releases/65258fe3619789033b447866/img/8665589-leaf-nature-icon-1-1.svg"
+            />
+            <div className="frame-5">
+              <div className="div-wrapper">
+                <div className="text-wrapper-9">STEM Learning</div>
+              </div>
+              <p className="text-wrapper-10">
+                Hydroponics offers a hands-on approach to learning, turning your classroom into a living lab. Students
+                gain experience in science, technology, engineering, and math (STEM) fields while learning about
+                sustainable agriculture.
+              </p>
+            </div>
+          </div>
+          <div className="frame-6">
+            <img
+              className="union"
+              alt="Union"
+              src="https://cdn.animaapp.com/projects/652581de01736f706d586056/releases/65258fe3619789033b447866/img/union.svg"
+            />
+            <div className="frame-7">
+              <div className="div-wrapper">
+                <div className="text-wrapper-11">Healthy Eating</div>
+              </div>
+              <p className="the-fresh-locally">
+                The fresh, locally-grown produce from our hydroponic units promotes healthy eating. It can be used in
+                the school&#39;s cafeteria, providing nutritious food options for students and staff. Surplus can also
+                be donated to local food bank initiatives.
+              </p>
+            </div>
+          </div>
+          <div className="frame-8">
+            <img
+              className="img"
+              alt="Element leaf nature"
+              src="https://cdn.animaapp.com/projects/652581de01736f706d586056/releases/65258fe3619789033b447866/img/8665589-leaf-nature-icon-1-2.svg"
+            />
+            <div className="frame-9">
+              <div className="div-wrapper">
+                <div className="text-wrapper-12">Skill Development</div>
+              </div>
+              <p className="text-wrapper-13">
+                Working with our hydroponic units helps students develop important skills like teamwork,
+                problem-solving, and critical thinking. These valuable skills not only enhance their academic
+                performance but also prepare them for future career success.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="frame-10">
+          <div className="frame-4">
+            <div className="frame-3">
+              <div className="div-wrapper">
+                <div className="text-wrapper-7">Sustainable Agriculture</div>
+              </div>
+              <p className="text-wrapper-8">
+                Our hydroponic units use up to 90% less water than traditional soil farming, making them a sustainable
+                choice for food production. Plus, they can be used indoors, reducing the need for transportation and
+                associated carbon emissions.
+              </p>
+            </div>
+          </div>
+          <div className="frame-wrapper">
+            <div className="frame-5">
+              <div className="div-wrapper">
+                <div className="text-wrapper-9">STEM Learning</div>
+              </div>
+              <p className="text-wrapper-10">
+                Hydroponics offers a hands-on approach to learning, turning your classroom into a living lab. Students
+                gain experience in science, technology, engineering, and math (STEM) fields while learning about
+                sustainable agriculture.
+              </p>
+            </div>
+          </div>
+          <div className="frame-11">
+            <div className="frame-7">
+              <div className="div-wrapper">
+                <div className="text-wrapper-14">Our Mission</div>
+              </div>
+              <p className="text-wrapper-15">
+                Empowering the next generation through sustainable hydroponic gardening for hands-on STEM learning and
+                positive community impact.
+              </p>
+            </div>
+          </div>
+          <div className="frame-12">
+            <div className="frame-13">
+              <div className="div-wrapper">
+                <div className="text-wrapper-12">Skill Development</div>
+              </div>
+              <p className="text-wrapper-13">
+                Working with our hydroponic units helps students develop important skills like teamwork,
+                problem-solving, and critical thinking. These valuable skills not only enhance their academic
+                performance but also prepare them for future career success.
+              </p>
+            </div>
+          </div>
+          <img
+            className="untitled-design-2"
+            alt="Untitled design"
+            src="https://cdn.animaapp.com/projects/652581de01736f706d586056/releases/65258fe3619789033b447866/img/untitled-design--3--1.png"
+          />
+        </div>
+        <div className="info-section">
+          <div className="rectangle-2" />
+          <div className="overlap-3">
+            <div className="rectangle-3" />
+            <p className="element-school-helped-and">
+              <span className="span">24</span>
+              <span className="text-wrapper-16">
+                &nbsp;&nbsp;School Helped <br />
+                and counting...
+              </span>
+            </p>
+          </div>
+          <div className="overlap-4">
+            <div className="rectangle-4" />
+            <p className="element-students">
+              <span className="span">500+</span>
+              <span className="text-wrapper-17">
+                {" "}
+                <br />
+              </span>
+              <span className="text-wrapper-16">students impacted</span>
+            </p>
+          </div>
+          <div className="rectangle-5" />
+        </div>
+        <div className="frame-14">
+          <p className="text-wrapper-18">Join the NLEats mailing list!</p>
+        </div>
+        <div className="group">
+          <div className="emailing-list">
+            <div className="text-wrapper-19">Subscribe!</div>
+          </div>
+          <div className="email-wrapper">
+            <input
+              className="email"
+              placeholder="&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;Email:"
+              type="email"
+            />
+          </div>
+        </div>
+        <div className="group-2">
+          <div className="frame-15">
+            <div className="frame-16">
+              <div className="frame-17">
+                <div className="frame-18">
+                  <div className="text-wrapper-20">Our Mission</div>
+                </div>
+                <p className="text-wrapper-21">
+                  Empowering the next generation through sustainable hydroponic gardening for hands-on STEM learning and
+                  positive community impact.
+                </p>
+              </div>
+              <div className="frame-19">
+                <div className="frame-20">
+                  <div className="text-wrapper-22">Our Vision</div>
+                </div>
+                <p className="text-wrapper-21">
+                  We envision a future where every school harnesses the power of hydroponic technology to provide fresh,
+                  locally-grown produce for their students and staff. Through this, we hope to nurture young minds who
+                  are well-equipped to lead our world towards a sustainable future.
+                </p>
+              </div>
+              <div className="frame-21">
+                <div className="frame-22">
+                  <div className="text-wrapper-23">Our Goals</div>
+                </div>
+                <p className="integrate">
+                  Integrate hydroponics into high school curriculums nationwide.
+                  <br />
+                  Enhance STEM learning with hands-on experience.
+                  <br />
+                  Make fresh, local produce more accessible in communities.
+                </p>
+              </div>
+            </div>
+            <div className="frame-23">
+              <div className="frame-17">
+                <div className="frame-20">
+                  <div className="text-wrapper-24">Our Impact</div>
+                </div>
+                <p className="text-wrapper-21">
+                  Since our inception, we&#39;ve equipped numerous schools with our modular hydroponic units, positively
+                  impacting thousands of students. Our initiative has not only transformed classrooms into vibrant,
+                  green spaces, but also made fresh, nutritious food more accessible in our communities.
+                </p>
+              </div>
+              <div className="frame-19">
+                <div className="frame-20">
+                  <div className="text-wrapper-25">Our Approach</div>
+                </div>
+                <p className="text-wrapper-21">
+                  We believe in the power of hands-on learning. That&#39;s why our program is designed to provide
+                  students with practical experience in setting up and maintaining their hydroponic units. We also
+                  support teachers with a comprehensive curriculum that ties the project to key concepts in science,
+                  technology, engineering, and math.
+                </p>
+              </div>
+              <div className="frame-24">
+                <div className="frame-20">
+                  <div className="text-wrapper-26">Our Community</div>
+                </div>
+                <img
+                  className="we-re-more-than-just"
+                  alt="We re more than just"
+                  src="https://cdn.animaapp.com/projects/652581de01736f706d586056/releases/65258fe3619789033b447866/img/we-re-more-than-just-a-program---we-re-a-community--we-connect-s.png"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
